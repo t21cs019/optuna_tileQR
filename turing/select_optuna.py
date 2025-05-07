5ac83846267605ece51307d206325cfd204ba402
t21cs019 <g25tk012@yamanashi.ac.jp>
2025-05-07 15:01:33 +0900

    [fix] fix variable objective関数内の変数の修正 main関数の関数名の修正

--- turing/select_optuna.py
+++ turing/select_optuna.py
@@ -2,29 +2,25 @@ import optuna
 import subprocess
 import re
 import time
-import csv
 import optuna.visualization as vis
 
 import sys
-
 sys.path.append('/home/kubota/Work/optuna_turing/tools')
 from run_time_dgeqrf import run_time_dgeqrf
-from optuna_base import visualize_study_results
 
 
 def objective(trial):
     size = 4096
-    min_nb = 192
     max_nb = 336
-    min_ib = 10
-
+    min_nb = 192
+    
     nb = 2 * trial.suggest_int("nb", min_nb/2, max_nb/2)
-    ib = 2 * trial.suggest_int("ib", min_ib/2, nb/4)
+    ib = 2 * trial.suggest_int("ib", 2, nb/4)
 
     return run_time_dgeqrf(16, size, nb, ib)
 
 
-def optuna_turing(n_trials):
+def select_turing(n_trials):
     # WSL上のOptunaの実行
     start = time.time()  # 現在時刻（処理開始前）を取得
 
@@ -39,10 +35,16 @@ def optuna_turing(n_trials):
 
 
 def main():
+    trial = 20
+
     # 処理にかかった時間を表示
-    time = optuna_turing(20)
+    study, time = select_optuna(trial)
+
+    print(study.best_trial)
     print(f"処理にかかった時間は {time} 秒です")
 
 if __name__ == "__main__":
     main()
+
+
 
