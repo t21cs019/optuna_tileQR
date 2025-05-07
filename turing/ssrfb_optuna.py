0a5b8739f61791c2ec037acc1e97a08b2bbcffb4
t21cs019 <g25tk012@yamanashi.ac.jp>
2025-05-07 15:05:50 +0900

    [fix] fix ssrfb_turing 他のチューニングの形式と同じになるよう修正

--- turing/ssrfb_optuna.py
+++ turing/ssrfb_optuna.py
@@ -4,9 +4,7 @@ import re
 import time
 
 import sys
-
 sys.path.append('/home/kubota/Work/optuna_turing/tools')
-# Replace 'some_function' with the actual function name you want to import
 from run_time_dgeqrf import run_time_dgeqrf
 
 
@@ -29,9 +27,8 @@ def find_best_ib(nb, min_ib, max_ib):
     best_ib = None
     best_time = float('inf')
 
-    for ib in range(min_ib, max_ib + 1):
-        if nb % ib != 0:
-            continue
+    for ib in range(min_ib, max_ib + 1, 2):
+        print(f"Testing nb={nb}, ib={ib}...")
 
         # Run NoFlush with the current ib
         try:
@@ -59,35 +56,44 @@ def find_best_ib(nb, min_ib, max_ib):
 
 def objective(trial):
     size = 4096
-    min_nb = 192
     max_nb = 336
-    min_ib = 10
+    min_nb = 192
+    
+    nb = 2 * trial.suggest_int("nb", min_nb/2, max_nb/2)
 
-
-    nb = 2 * trial.suggest_int("nb", min_nb//2, max_nb//2)
-
-    best_ib, best_time = find_best_ib(nb//2, min_ib, nb//4)
+    best_ib, best_time = find_best_ib(nb, 4, nb/2)
 
     if best_ib is not None:
         print(f"The best ib for nb={nb} is {best_ib} with time {best_time:.6f} seconds.")
     else:
         print(f"No valid ib found for nb={nb}.")
+        return 0
 
     return run_time_dgeqrf(16, size, nb, best_ib)
 
 
-
-if __name__ == "__main__":
+def ssrfb_turing(n_trials):
     # WSL上のOptunaの実行
     start = time.time()  # 現在時刻（処理開始前）を取得
 
     study = optuna.create_study(direction="maximize")
-    study.optimize(objective, n_trials=100)
+    study.optimize(objective, n_trials)
 
     end = time.time()  # 現在時刻（処理完了後）を取得
 
     time_diff = end - start  # 処理完了後の時刻から処理開始前の時刻を減算する
-    print("処理にかかった時間は")
-    print(time_diff)  # 処理にかかった時間データを使用
+
+    return study, time_diff
+
+
+def main():
+    trial = 20
+
+    # 処理にかかった時間を表示
+    study, time = ssrfb_turing(trial)
 
     print(study.best_trial)
+    print(f"処理にかかった時間は {time} 秒です")
+
+if __name__ == "__main__":
+    main()
