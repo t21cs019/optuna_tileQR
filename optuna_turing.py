06e3f50ab911b8b46c3f3ef752050b94e7824858
t21cs019 <g25tk012@yamanashi.ac.jp>
2025-05-07 15:52:20 +0900

    [add] other_turing 他2種類のチューニングの計測を行う関数を追加

--- optuna_turing.py
+++ optuna_turing.py
@@ -6,6 +6,8 @@ import optuna.visualization as vis
 import sys
 sys.path.append('/home/kubota/Work/optuna_turing/turing')
 from normal_optuna import normal_turing
+from select_optuna import select_turing
+from ssrfb_optuna import ssrfb_turing
 
 def measurements_normal_optuna(counts, trials):
     normal_resutl_dir = "/home/kubota/Work/optuna_turing/result/normal_optuna"
@@ -31,7 +33,54 @@ def measurements_normal_optuna(counts, trials):
         # 可視化を一括で実行し、画像を保存
         visualize_study_results(best_params, best_value, study, output_fig_dir)
          
-    
+def measurements_select_optuna(counts, trials):
+    select_resutl_dir = "/home/kubota/Work/optuna_turing/result/select_optuna"
+
+    for i in range(counts):
+        output_fig_dir = f"{select_resutl_dir}/{trials}/trial_{i + 1}"
+        output_csv_file = f"{select_resutl_dir}/{trials}/optuna_turing.csv"
+
+        # ディレクトリを作成（存在しない場合）
+        os.makedirs(output_fig_dir, exist_ok=True)
+
+        print(f"=== {i + 1}/{counts} 回目の最適化を開始します ===")
+        study, time = select_turing(trials)
+
+        # 最適なパラメータペアと性能値を抽出
+        best_trial = study.best_trial
+        best_params = best_trial.params
+        best_value = best_trial.value
+
+        # チューニング結果を記録
+        save_best_trial(i + 1, time, best_params, best_value, output_csv_file)
+
+        # 可視化を一括で実行し、画像を保存
+        visualize_study_results(best_params, best_value, study, output_fig_dir)
+
+def measurements_ssrfb_optuna(counts, trials):
+    ssrfb_resutl_dir = "/home/kubota/Work/optuna_turing/result/ssrfb_optuna"
+
+    for i in range(counts):
+        output_fig_dir = f"{ssrfb_resutl_dir}/{trials}/trial_{i + 1}"
+        output_csv_file = f"{ssrfb_resutl_dir}/{trials}/optuna_turing.csv"
+
+        # ディレクトリを作成（存在しない場合）
+        os.makedirs(output_fig_dir, exist_ok=True)
+
+        print(f"=== {i + 1}/{counts} 回目の最適化を開始します ===")
+        study, time = ssrfb_turing(trials)
+
+        # 最適なパラメータペアと性能値を抽出
+        best_trial = study.best_trial
+        best_params = best_trial.params
+        best_value = best_trial.value
+
+        # チューニング結果を記録
+        save_best_trial(i + 1, time, best_params, best_value, output_csv_file)
+
+        # 可視化を一括で実行し、画像を保存
+        visualize_study_results(best_params, best_value, study, output_fig_dir)
+
 
 def debag_print(best_params, best_value, time):        
     print(f"最適なパラメータ: nb={best_params['nb']}, ib={best_params['ib']}")
